8a5c1d1ce9be8ff49514648f37147e1c45cbcdc2
Jason Dunkelberger (aka dirkraft) <dirkraft@gmail.com>
2016-08-04 10:11:54 -0700

    i_0 through i_6

--- src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
+++ src/i_introduction/_1_Java_To_Kotlin_Converter/JavaToKotlinConverter.kt
@@ -13,5 +13,5 @@ fun todoTask1(collection: Collection<Int>): Nothing = TODO(
 
 
 fun task1(collection: Collection<Int>): String {
-    todoTask1(collection)
+    return JavaCodeConversion().task1(collection)
 }
