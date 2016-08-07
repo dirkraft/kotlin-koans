5166e046551911ce2e8b84a275598a2213b3aa6a
Jason Dunkelberger (aka dirkraft) <dirkraft@gmail.com>
2016-08-06 17:35:27 -0700

    vi
    was pretty heavy

--- src/vi_generics/_41_GenericFunctions.kt
+++ src/vi_generics/_41_GenericFunctions.kt
@@ -19,12 +19,20 @@ fun task41(): Nothing = TODO(
         }
 )
 
+fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(
+        truthyReceiver: C, falseyReceiver: C, parter: (T) -> Boolean)
+        : Pair<C, C> {
+
+    val (truthy, falsey) = this.partition { parter(it) }
+    truthyReceiver.addAll(truthy)
+    falseyReceiver.addAll(falsey)
+    return truthyReceiver to falseyReceiver
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
